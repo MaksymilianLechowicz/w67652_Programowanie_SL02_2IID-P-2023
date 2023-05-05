0ca7def95b9c1dd6b1ddc97064e5997af92dd72c
IceScarecrow <maxspore4444@gmail.com>
2023-05-05 13:23:08 +0200

    lab 8
    regex

--- lab78/formjs.js
+++ lab78/formjs.js
@@ -8,7 +8,7 @@ function requiredValidation(pole){
         return false;
     }
     else{
-        error.innerHTML = '';
+        error.innerHTML = 'ok';
         return true;
     }
 }
@@ -19,11 +19,69 @@ form.addEventListener('submit', event => {
     form.reset();
 }
 )
+function lengValidation(pole){
+    console.log(`[name='${pole.name}']+ span.lenError`)
+    const lenError = document.querySelector(`[name='${pole.name}'] + span.error`);
+    console.log(lenError)
+    if(pole.value.length >= 3){
+        lenError.innerHTML = 'ok';
+        return true;
+    }
+    else{
+        lenError.innerHTML = 'Długość Imienia lub Naziwska za krótka';
+        return false;
+    }
+}
 
+function emailValidation(pole){
+    console.log(`[name='${pole.name}']+ span.EmError`)
+    const EmError = document.querySelector(`[name='${pole.name}'] + span.error`);
+    console.log(EmError)
+    const reges = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; 
+    if (reges.test(pole.value)){
+        EmError.innerHTML = "ok"
+        return true
+    }
+    else{
+        EmError.innerHTML = "Niepoprawy Adres Email"
+        return false
+    }
+}
 const name = document.querySelector("[name='name']");
-
-
-name.addEventListener('input', ()=>{
+const surname = document.querySelector("[name='surname']");
+const email = document.querySelector("[name='email']")
+const pass = document.querySelector("[name='pass']")
+const sex = document.querySelector("[name='sex']")
+const tel = document.querySelector("[name='tel']")
+const dat = document.querySelector("[name='dat']")
+const country = document.querySelector("[name='country']")
+name.addEventListener('input', ()=>{    
     requiredValidation(name);
+    lengValidation(name)
 })
+surname.addEventListener('input', ()=>{
+    requiredValidation(surname);
+    lengValidation(surname)
+})
+
+email.addEventListener('input', ()=>{ 
+    requiredValidation(email);
+    emailValidation(email);
+})
+pass.addEventListener('input', ()=>{
+    requiredValidation(pass)
+})
+sex.addEventListener('input', ()=>{
+    requiredValidation(sex)
+})
+tel.addEventListener('input', ()=>{
+    requiredValidation(tel)
+})
+dat.addEventListener('input', ()=>{
+    requiredValidation(dat)
+})
+country.addEventListener('change', ()=>{
+    requiredValidation(country)
+})
+
 
